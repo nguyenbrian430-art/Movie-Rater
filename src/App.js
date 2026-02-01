ae6b89e1273936ad533998d3951e2c7b46c217f5
nguyenbrian430-art <nguyenbrian430@gmail.com>
2026-02-01 14:41:48 -0500

    Layout, List components, Fetching data, and CORS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import MovieList from "./components/movie-list";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Movie Rater</h1>
+        <div className="sideHeaders">
+          <MovieList />
+          <h1>Movie Details</h1>
+        </div>
       </header>
     </div>
   );
---
+++ src/components/movie-list.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+
+export default function MovieList() {
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:8000/api/movies/", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Token 9b2715743b984db9f4b6ae67f0efdfab9b0a453e"
+          },
+        });
+        if (!response.ok) {
+          setError("Error getting movies");
+          return;
+        }
+        const result = await response.json();
+        setMovies(result);
+      } catch {
+        setError("Error getting movies");
+      }
+    };
+    fetchMovies();
+  }, []);
+
+  if (error) return <h1>{error}</h1>;
+
+  return (
+    <div>
+      <h1>Movie List</h1>
+      {movies.map((movie) => {
+        return <h2 key={movie.id}>{movie.title}</h2>;
+      })}
+    </div>
+  );
+}
