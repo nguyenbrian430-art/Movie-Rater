eaa4ccbc1beb04fd4dc1a3963a2100967b06a258
nguyenbrian430-art <nguyenbrian430@gmail.com>
2026-02-02 17:57:04 -0500

    Made a display for ratings and for setting ratings

--- src/components/movie-details.jsx
+++ src/components/movie-details.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { FaRegStar, FaS, FaStar } from "react-icons/fa6";
 
 export default function MovieDetail({movie}) {
+
+    const [highlighted,setHighlighted]= useState(-1);
     
 
 
@@ -9,8 +11,23 @@ export default function MovieDetail({movie}) {
         <div>
             {movie && <div>
                 <h1>{movie.title}</h1>
-                <p>{movie.description}</p>     
-                <FaRegStar /> <FaStar/>
+                <p>{movie.description}</p>  
+                <div className="flex">   
+                    <FaStar className={movie.avg_rating > 0 && "starFill"}/>
+                    <FaStar className={movie.avg_rating > 1 && "starFill"}/>
+                    <FaStar className={movie.avg_rating > 2 && "starFill"}/>
+                    <FaStar className={movie.avg_rating > 3 && "starFill"}/>
+                    <FaStar className={movie.avg_rating > 4 && "starFill"}/>
+                    <p>({movie.no_of_ratings})</p>
+                </div>
+                <h1> Rate the Movie!</h1>
+                <div className="flex">
+                    {[...Array(5)].map( (el,indx)=>{
+                        return <FaStar className={highlighted > indx && "starRatingFill"}
+                            onMouseEnter={()=> setHighlighted(indx+1)} 
+                            onMouseLeave={() => setHighlighted(-1)}/>
+                    })}
+                </div>
             </div>}
         </div>
   );
