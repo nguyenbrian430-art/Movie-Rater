36e7afe63dd3ce269e96a8747a49bd7c247a6cd5
nguyenbrian430-art <nguyenbrian430@gmail.com>
2026-02-09 21:41:43 -0500

    Added Router, Login screen/system, and context/cookies

--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, {useState, useContext, useEffect} from "react";
+import API from "../services/api-service";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+//import {TokenContext} from "../index";
 
 export default function Auth(){
+
+    const [username,setUsername] = useState("");
+    const [password,setPassword] = useState("");
+
+    const [token,setToken] = useCookies("mr-token");
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        //console.log("token", token["mr-token"]);
+        if(token["mr-token"]) navigate('/movies');
+    },[token])
+
+    const loginUser = () => {
+        const getToken = async () =>{
+            const resp = await API.loginUser({username,password});
+            if(resp) setToken("mr-token",resp.token);
+        }
+        
+        getToken();
+    }
+
     return(
-        <h1>Auth component here</h1>
+        <div className="form">
+            <label htmlFor="username">Username</label>
+            <input id="username" type="text" placeholder="Username" value={username} onChange={(evt=>setUsername(evt.target.value))}/>
+
+            <label htmlFor="password">Password</label>
+            <input id="password" type="password" placeholder="password" value={password} onChange={(evt=>setPassword(evt.target.value))}/>
+
+            <button onClick={()=>loginUser()}>Login</button>
+            
+        </div>
     )
 }
--- src/services/api-service.js
+++ src/services/api-service.js
@@ -1,10 +1,10 @@
-const API_URL = "http://127.0.0.1:8000/api";
+const API_URL = "http://127.0.0.1:8000";
 const TOKEN = "9b2715743b984db9f4b6ae67f0efdfab9b0a453e";
 
 export default class API{
 
     static async fetchMovie(){
-        const response = await fetch(`${API_URL}/movies/`,{
+        const response = await fetch(`${API_URL}/api/movies/`,{
           method: "GET",
           headers: {
             "Content-Type": "application/json",
@@ -17,7 +17,7 @@ export default class API{
         return await response.json();
     }
     static async getMovie(movie_id){
-        const response = await fetch(`${API_URL}/movies/${movie_id}/`,{
+        const response = await fetch(`${API_URL}/api/movies/${movie_id}/`,{
           method: "GET",
           headers: {
             "Content-Type": "application/json",
@@ -30,7 +30,7 @@ export default class API{
         return await response.json();
     }
     static async rateMovie(movie_id,body){
-        const response = await fetch(`${API_URL}/movies/${movie_id}/rate_movie/`,{
+        const response = await fetch(`${API_URL}/api/movies/${movie_id}/rate_movie/`,{
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -44,7 +44,7 @@ export default class API{
         return await response.json();
     }
     static async updateMovie(movie_id, body){
-        const response = await fetch(`${API_URL}/movies/${movie_id}/`,{
+        const response = await fetch(`${API_URL}/api/movies/${movie_id}/`,{
           method: "PUT",
           headers: {
             "Content-Type": "application/json",
@@ -58,7 +58,7 @@ export default class API{
         return await response.json();
     }
     static async createMovie(body){
-        const response = await fetch(`${API_URL}/movies/`,{
+        const response = await fetch(`${API_URL}/api/movies/`,{
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -72,7 +72,7 @@ export default class API{
         return await response.json(); 
     }
     static async removeMovie(movie_id){
-        const response = await fetch(`${API_URL}/movies/${movie_id}/`,{
+        const response = await fetch(`${API_URL}/api/movies/${movie_id}/`,{
           method: "DELETE",
           headers: {
             "Content-Type": "application/json",
@@ -84,5 +84,17 @@ export default class API{
         }
         return true; 
     }
-
+    static async loginUser(body){
+        const response = await fetch(`${API_URL}/auth/`,{
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(body) 
+        });
+        if (!response.ok) {
+          return null;
+        }
+        return await response.json();
+    }
 }
